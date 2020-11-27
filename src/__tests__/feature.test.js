7c9c75502dd1258a06ef73895e83e78edd8331fa
Ryan Viegas <rjkviegas@gmail.com>
2020-11-27 11:39:34 +0000

    Feature Test for boxCalculator returns correct box for array of orders

--- src/__tests__/feature.test.js
+++ src/__tests__/feature.test.js
@@ -1,74 +1,145 @@
-const selectBox = require("../lib/selectBox");
+const { boxCalculator } = require("../lib/selectBox");
 
 describe("Feature test", () => {
-    describe("selectBox", () => {
-        it("returns a box big enough for an order", () => {
-            const boxes = [
+    const boxes = [
+        {
+            "id": "PK-MED-01",
+            "name": "Medium",
+            "dimensions": {
+                "widthMm": 30,
+                "heightMm": 50,
+                "depthMm": 60
+            },
+            "co2FootprintKg": 200
+        },
+        // boxVolMm3 = 90000
+        {
+            "id": "PK-SML-02",
+            "name": "Small",
+            "dimensions": {
+                "widthMm": 20,
+                "heightMm": 80,
+                "depthMm": 50
+            },
+            "co2FootprintKg": 100
+        },
+        // boxVolMm3 = 80000
+        {
+            "id": "PK-LRG-03",
+            "name": "Large",
+            "dimensions": {
+                "widthMm": 20,
+                "heightMm": 100,
+                "depthMm": 50
+          },
+          "co2FootprintKg": 300
+        }
+        // boxVolMm3 = 100000
+      ];
+    
+    describe("calculateBoxes", () => {
+        it("returns array of object containing order ids and box ids", () => {
+            const orders = [
                 {
-                    "id": "PK-MED-01",
-                    "name": "Medium",
-                    "dimensions": {
-                        "widthMm": 30,
-                        "heightMm": 50,
-                        "depthMm": 60
-                    },
-                    "co2FootprintKg": 200
-                },
-                // boxVolMm3 = 90000
-                {
-                    "id": "PK-SML-02",
-                    "name": "Small",
-                    "dimensions": {
-                        "widthMm": 20,
-                        "heightMm": 80,
-                        "depthMm": 50
-                    },
-                    "co2FootprintKg": 100
-                },
-                // boxVolMm3 = 80000
-                {
-                    "id": "PK-LRG-03",
-                    "name": "Large",
-                    "dimensions": {
-                        "widthMm": 20,
-                        "heightMm": 100,
-                        "depthMm": 50
-                  },
-                  "co2FootprintKg": 300
-                }
-                // boxVolMm3 = 100000
-              ];
-            const order = {
-                "id": "1",
-                "ingredients": [
-                    {
+                    "id": "1",
+                    "ingredients": [
+                        {
                         "name": "radishes",
                         "volumeCm3": 9
-                    },
-                    {
+                        },
+                        {
                         "name": "aubergine",
                         "volumeCm3": 18
-                    },
-                    {
+                        },
+                        {
                         "name": "super pasta",
                         "volumeCm3": 27
-                    },
-                    {
+                        },
+                        {
                         "name": "honey",
                         "volumeCm3": 7.2
-                    },
-                    {
+                        },
+                        {
                         "name": "duck",
                         "volumeCm3": 23
+                        }
+                  ]
+                },
+                // order volumeInMm3 = 84200
+                {
+                  "id": "2",
+                  "ingredients": [
+                    {
+                      "name": "artichokes",
+                      "volumeCm3": 20
+                    },
+                    {
+                      "name": "haricots",
+                      "volumeCm3": 6.7
+                    },
+                    {
+                      "name": "noodles",
+                      "volumeCm3": 18
+                    },
+                    {
+                      "name": "broccoli",
+                      "volumeCm3": 27.9
+                    },
+                    {
+                      "name": "mayonnaise",
+                      "volumeCm3": 3
                     }
-                ]
-              };
-            // order volumeInMm3 = 84200
-
-            expect(selectBox(boxes, order)).toEqual({
-                orderId: "1",
-                boxId: "PK-MED-01"
-            });
+                  ]
+                }
+                // order volumeInMm3 = 75600
+            ];
+            
+            expect(boxCalculator(boxes, orders)).toEqual([
+                {
+                    orderId: "1",
+                    boxId: "PK-MED-01"
+                },
+                {
+                    orderId: "2",
+                    boxId: "PK-SML-02"
+                }
+            ])
         })
-    })
+    });
+    
+    // describe("selectBox", () => {
+    //     it("returns a box big enough for an order", () => {
+    //         const order = {
+    //             "id": "1",
+    //             "ingredients": [
+    //                 {
+    //                     "name": "radishes",
+    //                     "volumeCm3": 9
+    //                 },
+    //                 {
+    //                     "name": "aubergine",
+    //                     "volumeCm3": 18
+    //                 },
+    //                 {
+    //                     "name": "super pasta",
+    //                     "volumeCm3": 27
+    //                 },
+    //                 {
+    //                     "name": "honey",
+    //                     "volumeCm3": 7.2
+    //                 },
+    //                 {
+    //                     "name": "duck",
+    //                     "volumeCm3": 23
+    //                 }
+    //             ]
+    //           };
+    //         // order volumeInMm3 = 84200
+
+    //         expect(selectBox(boxes, order)).toEqual({
+    //             orderId: "1",
+    //             boxId: "PK-MED-01"
+    //         });
+    //     })
+    // })
 })
--- src/lib/selectBox.js
+++ src/lib/selectBox.js
@@ -1,33 +1,41 @@
-function selectBox(boxes, order) {
-    const result = {};
-    result.orderId = order["id"];
-    const orderVolMm3 = calcOrderVol(order);
-    result.boxId = boxes.map(enrichBox)
-        .sort((a, b) => a.volMm3 - b.volMm3)
-        .filter(box => orderVolMm3 < box.volMm3)[0].id
-    return result;
+function boxCalculator(boxes, orders) {
+    return orders.map(selectBox)
 
-    function calcOrderVol(order) {
-        let result = 0;
-        order.ingredients.forEach(ingredient => {
-            result += ingredient.volumeCm3;
-        });
-        return result*1000;
-    }
-
-    function enrichBox(box) {
-        const result = Object.assign({}, box);
-        result.volMm3 = calcBoxVol(box);
-        return result
-    }
-
-    function calcBoxVol(box) {
-        let result = 1;
-        for (const dim in box.dimensions) {
-            result *= box.dimensions[dim];
-        }
+    function selectBox(order) {
+        const result = {};
+        result.orderId = order["id"];
+        const orderVolMm3 = calcOrderVol(order);
+        result.boxId = boxes.map(enrichBox)
+            .sort((a, b) => a.volMm3 - b.volMm3)
+            .filter(box => orderVolMm3 < box.volMm3)[0].id
         return result;
+    
+        function calcOrderVol(order) {
+            let result = 0;
+            order.ingredients.forEach(ingredient => {
+                result += ingredient.volumeCm3;
+            });
+            return result*1000;
+        }
+    
+        function enrichBox(box) {
+            const result = Object.assign({}, box);
+            result.volMm3 = calcBoxVol(box);
+            return result
+        }
+    
+        function calcBoxVol(box) {
+            let result = 1;
+            for (const dim in box.dimensions) {
+                result *= box.dimensions[dim];
+            }
+            return result;
+        }
     }
-}
+};
 
-module.exports = selectBox;
+
+
+module.exports = {
+    boxCalculator
+};
